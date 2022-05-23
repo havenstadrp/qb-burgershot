c83f36ffc4736e7f5e6497af2c5ba3e00f8e499d
bedar89 <bedar89@gmail.com>
2022-05-23 20:35:33 +0000

    Switch back to LegacyFuel

--- client/garage.lua
+++ client/garage.lua
@@ -72,7 +72,7 @@ AddEventHandler('qb-burgershot:garage', function(bs)
             if vehicle == 'stalion2' then		
                 QBCore.Functions.SpawnVehicle(vehicle, function(veh)
                     SetVehicleNumberPlateText(veh, "BURGER"..tostring(math.random(1000, 9999)))
-                    exports['cc-fuel']:SetFuel(veh, 100.0)
+                    exports['LegacyFuel']:SetFuel(veh, 100.0)
                     SetEntityHeading(veh, coords.w)
                     TaskWarpPedIntoVehicle(PlayerPedId(), veh, -1)
                     TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(veh))
